83e080971bcb00f4b0e3b7d5c050d6424d2ce298
tcaish <tim.caish@foolwealth.com>
2022-07-12 08:03:25 -0400

    testing stripe 1

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`);
 
 exports.handler = async (event) => {
+  console.log(process.env.STRIPE_PUBLISHABLE_KEY);
+  console.log(process.env.STRIPE_SECRET_KEY);
+  console.log(`${process.env.STRIPE_PUBLISHABLE_KEY}`);
+  console.log(`${process.env.STRIPE_SECRET_KEY}`);
   try {
     const { amount } = JSON.parse(event.body);
     const paymentIntent = await stripe.paymentIntents
