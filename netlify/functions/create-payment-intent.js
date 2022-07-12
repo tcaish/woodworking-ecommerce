7f422670da635caa5d1a04efe267f6698e133b8c
tcaish <tim.caish@foolwealth.com>
2022-07-12 07:41:26 -0400

    testing stripe

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -3,14 +3,14 @@ const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`);
 
 exports.handler = async (event) => {
   try {
-    console.log('yo 1');
     const { amount } = JSON.parse(event.body);
-    console.log('yo 2');
+    console.log('here 1', event.body);
     const paymentIntent = await stripe.paymentIntent.create({
       amount,
       currency: 'usd',
       payment_method_types: ['card']
     });
+    console.log('here 2');
     return {
       statusCode: 200,
       body: JSON.stringify({ paymentIntent })
