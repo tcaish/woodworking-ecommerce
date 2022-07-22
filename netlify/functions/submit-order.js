2a77cbdabc464a66584722b166127bf36efa1e07
tcaish <tim.caish@foolwealth.com>
2022-07-22 11:16:52 -0400

    testing stripe

--- netlify/functions/submit-order.js
+++ netlify/functions/submit-order.js
@@ -1,44 +1,20 @@
 // require('dotenv').config();
-const Stripe = require('stripe');
-const stripe = Stripe(`${process.env.STRIPE_SECRET_KEY}`, {
+const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`, {
   apiVersion: '2020-08-27; orders_beta=v4'
 });
 
 exports.handler = async (event) => {
   try {
     const { order_id, total } = JSON.parse(event.body);
-    const resource = Stripe.StripeResource.extend({
-      request: Stripe.StripeResource.method({
-        method: 'post',
-        path: `orders/${order_id}/submit`
-      })
+    const order = await stripe.orders.submit(order_id, {
+      expected_total: total
     });
 
-    const response = await new resource(stripe).request(
-      {
-        expected_total: total
-      },
-      function (err, res) {
-        if (res) {
-          return {
-            statusCode: 200,
-            body: JSON.stringify(res)
-          };
-        }
-
-        if (err) {
-          return {
-            statusCode: 400,
-            body: JSON.stringify(err)
-          };
-        }
-      }
-    );
-
-    console.log(response);
-    return response;
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ order })
+    };
   } catch (err) {
-    console.log(err);
     return {
       statusCode: 400,
       body: JSON.stringify({ err })
